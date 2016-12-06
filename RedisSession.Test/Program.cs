6de9841a7b26e44241cabc015a00ecd9cd765e86
syxtest01@gmail.com <xing_*666888>
2016-12-06 17:39:13 +0800

    新建项目

---
+++ RedisSession.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisSession.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RedisClient("192.168.137.133", 6379);
+            client.Set<string>("name", "test1");
+        }
+    }
+}
